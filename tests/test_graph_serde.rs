4997394bf9a328e2061be96a4a3344400e131d4f
Nick Hynes <nhynes@berkeley.edu>
2018-05-28 20:59:46 +0000

    Update tests

--- tests/test_graph_serde.rs
+++ tests/test_graph_serde.rs
@@ -5,42 +5,48 @@ extern crate serde_json;
 
 extern crate tvm;
 
-use std::{convert::TryFrom, fs, io::Read};
+use std::{collections::HashMap, convert::TryFrom, fs, io::Read};
 
-#[test]
-fn test_resnet_inference() {
-  let graph = tvm::runtime::Graph::try_from(&fs::read_string(concat!(
-    env!("CARGO_MANIFEST_DIR"),
-    "/tests/resnet_inference.json"
-  )).unwrap())
+use tvm::{
+  errors::*,
+  runtime::{Graph, GraphExecutor, SystemLibModule, Tensor},
+};
+
+fn load_graph() -> Result<Graph> {
+  Graph::try_from(
+    &fs::read_string(concat!(env!("CARGO_MANIFEST_DIR"), "/tests/graph.json")).unwrap(),
+  )
+}
+
+fn load_params() -> Result<HashMap<String, Tensor>> {
+  let mut params_bytes = Vec::new();
+  fs::File::open(concat!(env!("CARGO_MANIFEST_DIR"), "/tests/graph.params"))
+    .unwrap()
+    .read_to_end(&mut params_bytes)
     .unwrap();
-
-  assert_eq!(graph.nodes[0].name, "data");
-  assert_eq!(
-    graph.nodes[1]
-      .attrs
-      .as_ref()
-      .unwrap()
-      .get("epsilon")
-      .cloned()
-      .unwrap(),
-    "2e-05"
-  );
-  assert_eq!(graph.nodes[5].inputs[3].version, 1);
-  assert_eq!(
-    graph.attrs.as_ref().unwrap().get("shape").unwrap()[1][0][3],
-    224
-  );
+  tvm::runtime::load_param_dict(&params_bytes)
 }
 
 #[test]
-fn test_resnet_inference_params() {
-  let mut params_bytes = Vec::new();
-  fs::File::open(concat!(
-    env!("CARGO_MANIFEST_DIR"),
-    "/tests/resnet_inference.params"
-  )).unwrap()
-    .read_to_end(&mut params_bytes)
-    .unwrap();
-  assert!(tvm::runtime::load_param_dict(&params_bytes).is_ok());
+fn test_load_graph() {
+  let graph = load_graph().unwrap();
+
+  assert_eq!(graph.nodes[3].op, "tvm_op");
+  assert_eq!(
+    graph.nodes[3]
+      .attrs
+      .as_ref()
+      .unwrap()
+      .get("func_name")
+      .unwrap(),
+    "fuse_dense"
+  );
+  assert_eq!(graph.nodes[5].inputs[0].index, 0);
+  assert_eq!(graph.nodes[6].inputs[0].index, 1);
+  assert_eq!(graph.heads.len(), 2);
+}
+
+#[test]
+fn test_load_params() {
+  assert!(load_params().is_ok());
 }
